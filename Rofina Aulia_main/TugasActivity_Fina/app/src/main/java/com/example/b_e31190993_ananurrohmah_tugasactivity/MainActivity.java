7ad1e645bec904321ba475a1ff725265b0dd499f
Rofina Aulia Noviati <65754779+Rofinaaulia@users.noreply.github.com>
2021-06-14 10:25:47 +0700

    Rofina Aulia_main

---
+++ Rofina Aulia_main/TugasActivity_Fina/app/src/main/java/com/example/b_e31190993_ananurrohmah_tugasactivity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.b_e31190993_ananurrohmah_tugasactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
